1e81fc04235009ef54f581aa7362c661c297f10e
Maike <maikeadolph@gmx.de>
2020-05-22 17:26:45 +0200

    Informations get written in DB

--- src/anmelden.php
+++ src/anmelden.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<form class="container" method="post">
+<form class="container" action= "anmelden_logic.php" method="post">
 
       <div class="form-row">
         <div class="form-group col-md-12">
--- src/anmelden_logic.php
+++ src/anmelden_logic.php
@@ -3,5 +3,33 @@
 
   $nickname = $_POST['inputName'];
   $email = $_POST['inputEmail'];
-  $password = $_POST['inputPassword'];
+  $password = password_hash($_POST['inputPassword'], PASSWORD_DEFAULT);
+
+  //looking if email exist
+  $sql_email = $connection->prepare('SELECT * FROM Nutzer WHERE Email=?');
+  $sql_email->bind_param('s', $email );
+  $sql_email->execute();
+  $sql_email_results = $sql_email->get_result();
+
+  //looking if nickname exist
+  $sql_nickname = $connection->prepare('SELECT * FROM Nutzer WHERE Nickname=?');
+  $sql_nickname->bind_param('s', $nickname );
+  $sql_nickname->execute();
+  $sql_nickname_results = $sql_nickname->get_result();
+
+  //if email or nickname does not exist everything gets written in the DB
+if($sql_email_results->num_rows == 0 && $sql_nickname_results->num_rows ==0 ){
+    $sql_insert = $connection->prepare('INSERT INTO Nutzer(Nickname, Email, Passwort) VALUES(?,?,?)');
+    $sql_insert->bind_param('sss', $nickname, $email, $password);
+    $sql_insert->execute();
+    header('location: landing.php');
+  }else{
+    header('location: landing.php');
+    exit();
+  }
+
+  $connection->close();
+  $sql_email->close();
+  $sql_nickname->close();
+  $sql_insert->close();
  ?>
