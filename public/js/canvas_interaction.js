3dc5f58cf6eb4f8cae05c690fea0819dcd2a1852
Chris K <chr.kam@web.de>
2020-06-21 16:04:54 +0200

    removed not working code , adjusted some code to be more generous

--- public/js/canvas_interaction.js
+++ public/js/canvas_interaction.js
@@ -5,11 +5,7 @@
  *
  * TODO: Highlighting, moveChesspiece, 
  */
-var CHESSPIECE_SELECTED = false;
 var IMGDATA_BEFORE_HIGHLIGHTING = false;
-var SELECTED_X = false;
-var SELECTED_Y = false;
-var CURRENTLY_MOVING = false;
 
 function highlighting(event,boardstate){
 	var canvas = $("#chess")[0];
@@ -24,8 +20,6 @@ function highlighting(event,boardstate){
 	var mouseCoordY = 0;
 	mouseCoordX = event.clientX;
 	mouseCoordY = event.clientY;
-	
-	
 	
 	var coordX = 0;
 	var coordY = 0;
@@ -61,62 +55,22 @@ function resetHighlighting(){
 	ctx.putImageData(IMGDATA_BEFORE_HIGHLIGHTING,0,0);
 }
 function checkIfChesspieceShouldMove(event,boardstate){
-	var canvas = $("#chess")[0];
-	var mouseCoordX = 0;
-	var mouseCoordY = 0;
-	mouseCoordX = event.clientX;
-	mouseCoordY = event.clientY;
-	var coordX = 0;
-	var coordY = 0;
-	coordX = Math.floor(mouseCoordX/(canvas.width/8));
-	coordY = Math.floor(mouseCoordY/(canvas.height/8));
-	if(CHESSPIECE_SELECTED != false && CURRENTLY_MOVING == false){
-		if(SELECTED_X == coordX && SELECTED_X == coordY){
-			resetHighlighting();
-			CHESSPIECE_SELECTED = false;
-			SELECTED_Y = false;
-			SELECTED_X = false;
-		}
-		else if (boardstate[SELECTED_Y][SELECTED_X].color == boardstate[coordY][coordX].color){
-			resetHighlighting();
-			CHESSPIECE_SELECTED = true;
-			SELECTED_Y = coordY;
-			SELECTED_X = coordX;
-			highlightChesspiece(boardstate[coordY][coordX]);
-			
-		} 
-		else if (SELECTED_Y != false && SELECTED_X != false){
-			for(var element of boardstate[SELECTED_Y][SELECTED_X].moves){
-				if(element[1] == coordY && element[0] == coordX){
-					resetHighlighting();
-					CURRENTLY_MOVING = true;
-					moveChesspiece(boardstate,coordY,coordX);
-					CHESSPIECE_SELECTED = false;
-					SELECTED_Y = false;
-					SELECTED_X = false;
-					IMGDATA_BEFORE_HIGHLIGHTING = false;
-				} else {
-					resetHighlighting();
-					CHESSPIECE_SELECTED = false;
-					SELECTED_Y = false;
-					SELECTED_X = false; 
-				}
-			}
-		}
-	}
 }
 //TODO Implement
-function moveChesspiece(boardstate,yAfter,xAfter){
+function moveChesspiece(boardstate,yAfter,xAfter,yBefore,xBefore){
 	var canvas = $("#chess")[0];
 	var ctx = canvas.getContext("2d");
 	//moves Chesspiece
-	boardstate[yAfter][xAfter] = boardstate[SELECTED_Y][SELECTED_X];
-	boardstate[SELECTED_Y][SELECTED_X] = "";
+	boardstate[yAfter][xAfter] = boardstate[yBefore][xBefore];
+	boardstate[yAfter][xAfter].row = yAfter;
+	boardstate[yAfter][xAfter].column = xAfter;
+	boardstate[yBefore][xBefore] = "";
 	//Resets Fields on Board and draws Chesspiece at right place
-	drawField(SELECTED_Y,SELECTED_X);
+	drawField(yBefore,xBefore);
 	drawField(yAfter,xAfter);
 	drawPiece(boardstate[yAfter][xAfter],xAfter*canvas.height/8,yAfter*canvas.width/8);
-	CURRENTLY_MOVING = false;
-	setMovesOfChesspieces(boardstate);
-	console.log(boardstate);
+	boardstate[xAfter][yAfter].setMoves(boardstate);
+	//highlightChesspiece(boardstate[xAfter][yAfter]);
+	//console.log(boardstate[yAfter][xAfter].moves);
+	//console.log(boardstate);
 }
